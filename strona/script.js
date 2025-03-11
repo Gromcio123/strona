43d41e61d83bda7a9c30a2c43e72e01907703779
Gromcio123 <wiktor.gromada@interia.pl>
2025-03-11 02:11:26 +0100

    Update script.js

--- strona/script.js
+++ strona/script.js
@@ -2,17 +2,17 @@
 document.getElementById('contactForm').addEventListener('submit', function(event) {
     event.preventDefault();
 
-    // Pobieramy wartości wprowadzonych danych
+   
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
     const message = document.getElementById('message').value;
 
-    // Sprawdzamy, czy wszystkie pola są wypełnione
+   
     if (name === '' || email === '' || message === '') {
         alert('Proszę wypełnić wszystkie pola formularza.');
     } else {
-        // Jeśli wszystko jest OK, wyświetlamy komunikat i resetujemy formularz
-        document.getElementById('responseMessage').style.display = 'block'; // Wyświetlamy wiadomość
-        document.getElementById('contactForm').reset(); // Opcjonalnie: czyścimy formularz
+        
+        document.getElementById('responseMessage').style.display = 'block'; 
+        document.getElementById('contactForm').reset(); 
     }
 });
